2664c9cb7cca83be161b7740b1855130da8e9a7e
Chien-Ting <chienting@gmail.com>
2019-06-25 13:42:13 -0500

    update martexpmap to match with input image

--- martexpmap.py
+++ martexpmap.py
@@ -18,130 +18,19 @@ from scipy.interpolate import griddata as gd
 from scipy.interpolate import interp1d
 from astropy import wcs
 
-
 class HelpfulParser(argparse.ArgumentParser):
+	'''
+	Handling errors
+	'''
 	def error(self, message):
 		sys.stderr.write('error: %s\n' % message)
-		self.print_help()
+		sys.stderr.write('run \"python martexpmap.py -h\" for helps\n')
 		sys.exit(2)
 
-parser = HelpfulParser(description=__doc__,
-	epilog="""Chien-Ting Chen <ct.chen@nasa.gov>""",
-	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-
-# disabled for now. - CTC 20190620
-# parser.add_argument('--xmlname', type=str, required=False,
-# help='name of the telescope instrument specifications, an XML file.\n If not specified, the fov parameter should be specified.').
-#
-
-# Required arguments
-parser.add_argument('--attname', type=str, required=True,
-	help='Name of the attitude file.')
-
-parser.add_argument('--outname', type=str, required=True, default='expmap.fits',
-	help='Name of the output file.')
-
-parser.add_argument('--fov', type=float, required=True,
-	help='Field-of-view of the telescope to be simulated in arcmin. Supercedes the XML settings.')
-
-parser.add_argument('--rasize', type=float, required=True, default=30.,
-	help='RA pixel size in arcsec.')
-
-parser.add_argument('--decsize', type=float, required=True, default=30.,
-	help='DEC pixel size in arcsec.')
-
-
-# optional arguments
-
-parser.add_argument('--vigname', type=str, required=False,
-	help='Name of the vignetting file. Only a fits file in compliance with the OGIP standard is supported (CAL/GEN/92-021). \
-	If not provided, the script would only calculate the "raw" exposure map.')
-
-parser.add_argument('--ra1', type=float, required=False,
-	help='RA lower limit (in deg) for the area of interests.')
-
-parser.add_argument('--ra2', type=float, required=False,
-	help='RA upper limit (in deg) for the area of interests.')
-
-parser.add_argument('--dec1', type=float, required=False,
-	help='DEC lower limit (in deg) for the area of interests.')
-
-parser.add_argument('--dec2', type=float, required=False,
-	help='DEC upper limit (in deg) for the area of interests.')
-
-parser.add_argument('--energy', type=float, required=False,
-	help='The user can specify the energy from which the vignetting function the script should use. Must specify --vigname to do this. \
-	If none is provided, the lowest energy vignetting function is used')
-
-parser.add_argument('--attres', type=float, required=False,
-	help='Time resolution in seconds. The input attitude file can be rebinned or interpolated at the supplied time resolution.\
-	Re-binning/interpolating can speed up or slow down the computation time significantly.')
-
-parser.add_argument('--time', type=float, required=False,
-	help='Add a time constraint to the attitude file. Only the periods within the specified time would be considered.')
-
-args = parser.parse_args()
-
-print(args)
-attname = args.attname
-vigname = args.vigname
-outname = args.outname
-rasize = args.rasize
-decsize = args.decsize
-energy = args.energy
-attres = args.attres
-timelim = args.time
-fov = args.fov
-fovdeg = fov / 60.
-
-# Get the vignetting function at the designated energy --
-# only if a vignetting file name is provided
-if vigname is not None:
-	print('Vignetting function will be used.')
-	vigtab = fits.getdata(vigname)
-	vig_theta = vigtab['THETA'][0]
-	if len(np.shape(vigtab['VIGNET'][0])) == 2:
-		vig_vig = vigtab['VIGNET'][0]
-	elif len(np.shape(vigtab['VIGNET'][0])) == 3:
-		vig_vig = vigtab['VIGNET'][0][0]
-	vig_elo = vigtab['ENERG_LO'][0]
-	vig_ehi = vigtab['ENERG_HI'][0]
-	vig_emed = (vig_elo + vig_ehi) / 2.
-
-# interpolate the vignetting function at the input energy
-	grid_e, grid_th = np.meshgrid(np.array([energy]), vig_theta)
-	points = np.transpose(
-	                      [np.tile(vig_emed, len(vig_theta)), \
-	                       np.repeat(vig_theta, len(vig_emed))]
-	                       )
-	values = vig_vig.flatten()
-	int_vig = gd(points, values, (grid_e, grid_th), method='nearest').flatten()
-
-	vigfunc = interp1d(vig_theta*60., int_vig)
-
-	def vig2d(ra, dec, pnt):
-		'''
-		For a given pointing position,
-		calculate the corresponding Vignetting values at the input RA/DEC values.
-		ra = x-axis
-		dec = y-axis
-		pnt = pointing position, with array([ra_pnt, dec_pnt]), in degrees
-		'''
-		pntra, pntdec = pnt
-		radist = ra - pntra
-		decdist = dec - pntdec
-		dist = np.sqrt((radist)**2 + (decdist)**2) * 60.  # in arcmin
-		vigout = np.zeros_like(dist)
-		vigout = vigfunc(dist)
-		return vigout
-else:
-	print('Vignetting function NOT found, calculate a raw exposure map.')
-
-
-# The function for rebinning the attitude file
 def rebinatt(atttime, attra, attdec, attres):
 	'''
+	The function to rebinning the attitude file	
 	#dist -- not used for now.
 	Rebin the ATT array according to the new attres.
 	Based on this scipy coockbook:
@@ -170,32 +59,176 @@ def rebinatt(atttime, attra, attdec, attres):
 		return newatttime, newattra, newattdec
 
 
+
+parser = HelpfulParser(description=__doc__,
+	epilog="""Chien-Ting Chen <ct.chen@nasa.gov>""",
+	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+
+# disabled for now. - CTC 20190620
+# parser.add_argument('-xmlname', type=str, required=False,
+# help='name of the telescope instrument specifications, an XML file.\n If not specified, the fov parameter should be specified.').
+#
+
+# Required arguments
+parser.add_argument('-attname', type=str, required=True,
+	help='Name of the attitude file.')
+
+parser.add_argument('-outname', type=str, required=True, default='expmap.fits',
+	help='Name of the output file.')
+
+parser.add_argument('-fov', type=float, required=True,
+	help='Field-of-view of the telescope to be simulated in arcmin. Supercedes the XML settings.')
+
+
+# optional arguments
+
+parser.add_argument('-vigname', type=str, required=False,
+	help='Name of the vignetting file. Only a fits file in compliance with the OGIP standard is supported (CAL/GEN/92-021). \
+	If not provided, the script would only calculate the "raw" exposure map.')
+
+parser.add_argument('-imgname', type=str, required=False,
+	help='Name of an image file, the pixel size, RA/DEC range, and WCS keywords will be used. \
+	If not provided, must specify all the following RA/DEC pixel sizes.')
+
+parser.add_argument('-rasize', type=float, required=False, default=30.,
+	help='RA pixel size in arcsec.')
+
+parser.add_argument('-decsize', type=float, required=False, default=30.,
+	help='DEC pixel size in arcsec.')
+
+parser.add_argument('-box', '-arg', nargs='+', type=float, default=[],
+	help="""list of ra dec values specifying the 4 corners of the box.
+	Example: -box ra1 ra2 dec1 dec2""")
+
+parser.add_argument('-energy', type=float, required=False,
+	help='The user can specify the energy from which the vignetting function the script should use. Must specify -vigname to do this. \
+	If none is provided, the lowest energy vignetting function is used')
+
+parser.add_argument('-attres', type=float, required=False,
+	help='Time resolution in seconds. The input attitude file can be rebinned or interpolated at the supplied time resolution.\
+	Re-binning/interpolating can speed up or slow down the computation time significantly.')
+
+parser.add_argument('-time', type=float, required=False,
+	help='Add a time constraint to the attitude file. Only the periods within the specified time would be considered.')
+
+parser.add_argument('-verbose', type=bool, required=False, default=True
+	help='Add a time constraint to the attitude file. Only the periods within the specified time would be considered.')
+
+args = parser.parse_args()
+
+verb = args.verbose
+attname = args.attname
+vigname = args.vigname
+imgname = args.imgname
+outname = args.outname
+rasize = args.rasize
+decsize = args.decsize
+energy = args.energy
+attres = args.attres
+timelim = args.time
+fov = args.fov
+fovdeg = fov / 60.
+
+if verb:
+	print('arguments passed:\n')
+	print(args)
+
+
+# Read attitude file 
 atttab = fits.getdata(attname)
 
-# use the RA/DEC range in the attitude files
-if args.ra1 is None:
-	ra1= np.min(atttab['RA'])
-if args.ra2 is None:
-	ra2= np.max(atttab['RA'])
-if args.dec1 is None:
-	dec1= np.min(atttab['DEC'])
-if args.dec1 is None:
-	dec2= np.max(atttab['DEC'])
 
-npixra = np.ceil((ra2 - ra1) / (rasize/3600.)).astype(int)
-npixdec = np.ceil((dec2 - dec1) / (decsize/3600.)).astype(int)
-ragrid = np.linspace(ra1, ra2, npixra)
-decgrid = np.linspace(dec1, dec2, npixdec)
-ras, decs = np.meshgrid(ragrid, decgrid)
-ras_1d = ras.flatten()
-decs_1d = decs.flatten()
+# Get the vignetting function at the designated energy --
+# only if a vignetting file name is provided
+if vigname is not None:
+	if verb:print('Vignetting function will be used.')
+	vigtab = fits.getdata(vigname)
+	vig_theta = vigtab['THETA'][0]
+	if len(np.shape(vigtab['VIGNET'][0])) == 2:
+		vig_vig = vigtab['VIGNET'][0]
+	elif len(np.shape(vigtab['VIGNET'][0])) == 3:
+		vig_vig = vigtab['VIGNET'][0][0]
+	vig_elo = vigtab['ENERG_LO'][0]
+	vig_ehi = vigtab['ENERG_HI'][0]
+	vig_emed = (vig_elo + vig_ehi) / 2.
+	# interpolate the vignetting function at the input energy
+	grid_e, grid_th = np.meshgrid(np.array([energy]), vig_theta)
+	points = np.transpose(
+	                      [np.tile(vig_emed, len(vig_theta)), \
+	                       np.repeat(vig_theta, len(vig_emed))]
+	                       )
+	values = vig_vig.flatten()
+	int_vig = gd(points, values, (grid_e, grid_th), method='nearest').flatten()
 
+	vigfunc = interp1d(vig_theta*60., int_vig)
+
+	def vig2d(ra, dec, pnt):
+		'''
+		For a given pointing position,
+		calculate the corresponding Vignetting values at the input RA/DEC values.
+		ra = x-axis
+		dec = y-axis
+		pnt = pointing position, with array([ra_pnt, dec_pnt]), in degrees
+		'''
+		pntra, pntdec = pnt
+		radist = ra - pntra
+		decdist = dec - pntdec
+		dist = np.sqrt((radist)**2 + (decdist)**2) * 60.  # in arcmin
+		vigout = np.zeros_like(dist)
+		vigout = vigfunc(dist)
+		return vigout
+else:
+	if verb:print('Vignetting function NOT found, calculate a raw exposure map.')
+
+
+
+# Get RA/DEC range and pixel sizes
+if imgname is not None:
+	# This would override all radec related arguments
+	ihdu = fits.open(imgname)
+	if len(ihdu) == 1:
+		img = ihdu[0].data
+		hdr = ihdu[0].header
+	else:
+		img = ihdu[1].data
+		hdr = ihdu[1].header
+	npixra = hdr['NAXIS1']
+	npixdec = hdr['NAXIS2']
+	rasize = hdr['CDELT1'] * 3600.
+	decsize = hdr['CDELT2'] * 3600.
+	ra1 = hdr['CRVAL1'] - rasize * (hdr['CRPIX1'] - 1)
+	ra2 = hdr['CRVAL1'] + rasize * (ranpix - hdr['CRPIX1'])
+	dec1 = hdr['CRVAL2'] - rasize * (hdr['CRPIX2'] - 1)
+	dec2 = hdr['CRVAL2'] + rasize * (decnpix - hdr['CRPIX2'])
+	w = WCS(hdr)
+	header_out = w.to_header()
+elif (len(args.box) == 4) | (len(args.box) == 0):
+	if len(args.box) == 4:
+		ra1, ra2, dec1, dec2 = args.box
+	else:
+		box = None
+		ra1 = np.min(atttab['RA'])
+		ra2= np.max(atttab['RA'])
+		dec1= np.min(atttab['DEC'])
+		dec2= np.max(atttab['DEC'])
+	npixra = np.ceil((ra2 - ra1) / (rasize/3600.)).astype(int)
+	npixdec = np.ceil((dec2 - dec1) / (decsize/3600.)).astype(int)
+	# define WCS 
+	w = wcs.WCS(naxis=2)
+	w.wcs.crpix = [npixra/2.,npixdec/2.]
+	w.wcs.cdelt = np.array([rasize/3600., decsize/3600.])
+	w.wcs.crval = [np.median(newattra), np.median(newattdec)]
+	w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+	header_out = w.to_header()
+else len(args.box) != 4:
+	parser.error('Must provide four radec values (e.g., python test_args.py -box ra1 ra2 dec1 dec2)')
+
+# work only with attitude entries within the area of interests
 atttab = atttab[(atttab['RA'] >= ra1) & (atttab['RA'] <= ra2) & \
                 (atttab['DEC'] >= dec1) & (atttab['DEC'] <= dec2)]
-
 if timelim is not None:
 	atttab = atttab[atttab['TIME'] <= atttab['TIME'][0] + timelim]
-
 atttime = atttab['TIME']
 attra = atttab['RA']
 attdec = atttab['DEC']
@@ -211,12 +244,18 @@ else:
 newatttime, newattra, newattdec = rebinatt(atttime, attra, attdec, attres)
 del(atttime, attra, attdec)
 
+
 # Work on the meshgrid -- use cKDTree to find the mesh points that's 
 # within the FOV radius at a given pointing position
+ragrid = np.linspace(ra1, ra2, npixra)
+decgrid = np.linspace(dec1, dec2, npixdec)
+ras, decs = np.meshgrid(ragrid, decgrid)
+ras_1d = ras.flatten()
+decs_1d = decs.flatten()
+
 coordinates = np.c_[ras.ravel(), decs.ravel()]
 tree = spatial.cKDTree(coordinates)
-
-expmap = np.zeros(np.shape(coordinates)[0])
+expmap = np.zeros(npixra * npixdec)
 
 '''
 Loop through all attitude entries
@@ -239,14 +278,6 @@ else:
         pnt = np.array([newattra[aid],newattdec[aid]])
         ix = tree.query_ball_point(pnt, fovdeg)
         expmap[ix] += vig2d(ras_1d[ix], decs_1d[ix], pnt) * attres
-
-# define WCS 
-w = wcs.WCS(naxis=2)
-w.wcs.crpix = [npixra/2.,npixdec/2.]
-w.wcs.cdelt = np.array([rasize/3600., decsize/3600.])
-w.wcs.crval = [np.median(newattra), np.median(newattdec)]
-w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
-header = w.to_header()
 
 # header is an astropy.io.fits.Header object.  We can use it to create a new
 # PrimaryHDU and write it to a file.
