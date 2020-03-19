212f5ed69b8360875704f8cae8ddfd0ef2b2c229
CTJChen <chienting@gmail.com>
2020-03-19 10:52:59 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 
-setup(name='martxc',
+setup(name='martxc_local',
       version='0.1',
       author='Chien-Ting Chen',
       author_email='ct.chen@nasa.gov',
@@ -16,7 +16,8 @@ setup(name='martxc',
       description='A collection of ART-XC data analysis and reduction scripts',
       packages=['martxclib'],
       scripts=['martexpmap.py', 'martevt2img.py', 'martspecextract.py','martspecextract_raw.py',\
-      'martpoisson.py', 'martmkarf.py', 'martfilter.py', 'martpha2pi.py', 'marteef.py', 'martmerge.py'],
+      'martpoisson.py', 'martmkarf.py', 'martfilter.py', 'martpha2pi.py', 'marteef.py', 
+      'martmerge.py', 'martfindtile.py'],
       long_description=long_description,
       install_requires=[
           "scipy",
